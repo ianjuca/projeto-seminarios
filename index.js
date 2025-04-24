5b7a0d19ca15fe24932ead6e28747bcb606ea7a5
ian jucá <ianrodriguesjuca@gmail.com>
2025-04-24 01:39:05 -0300

    trabalho seminarios primeiro commit

---
+++ index.js
@@ -0,0 +1,27 @@
+function clickVenda() {
+    location.href = "pagina-venda.html"
+}
+
+function clickCompra() {
+    location.href = "pagina-compra.html"
+}
+
+function clickVoltar() {
+    location.href = "index.html"
+}
+
+function clickComida(){
+    location.href = "pagina-compra-comida.html"
+}
+
+function clickVoltarCompra(){
+    location.href = "pagina-compra.html"
+}
+
+function clickBebida(){
+    location.href = "pagina-compra-bebida.html"
+}
+
+function clickLimpeza(){
+    location.href = "pagina-compra-limpeza.html"
+}
